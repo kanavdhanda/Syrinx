1407e2309aa38dbd140660e83781f920a3cd5695
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-07-18 04:37:31 +0530

    layout almost complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 
 import Login from "./Pages/Login"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
 import Levels from "./Pages/levels"
+import Level from "./Pages/level"
+import Leaderboard from "./Pages/leaderboard"
+import Home from "./Pages/Home"
 function App() {
 
+  const isAuthenticated = true;
   
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/lvls" element={isAuthenticated? <Levels />:<Login />} />
+          <Route path="/home" element={<Home />} />
+
+          <Route path="/lvls" element={isAuthenticated? <Levels/>:<Login />} />
+          <Route path="/lvls/:id" element={<Level/>} />
+          <Route path="/leaderboard" element={isAuthenticated? <Leaderboard/>:<Login />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+
+          
         </Routes>
       </Router>
     </>
--- src/Pages/level.jsx
+++ src/Pages/level.jsx
@@ -1,13 +1,35 @@
-import React, { useState } from "react";   
-export default function Level(){
-    const [isPaused, setIsPaused] = useState(false);
-   return( <div className="relative w-full h-full">
-        <button className="absolute inset-0" onClick={()=>setIsPaused(!isPaused)}>Pause</button>
-        <div className="absolute inset-0 z-10" style={{backgroundColor: isPaused ? "rgba(0,0,0,0.5)" : "transparent"}}>
+import  { useState } from "react";
+import GdLoader from "../Components/gdLoader";
+import { useParams } from "react-router-dom";
+export default function Level() {
+  let { id } = useParams();
+  let gameId = parseInt(id, 10);
+  const [isPaused, setIsPaused] = useState(false);
+  return (
+    <div className="relative w-full h-full">
+         {/* Change the text to button and style it */}
+      <button
+        className="absolute inset-0"
+        onClick={() => setIsPaused(!isPaused)}
+      >
+        Pause
+      </button>
+      <div
+        className="absolute inset-0 z-10"
+        style={{
+          backgroundColor: isPaused ? "rgba(0,0,0,0.5)" : "transparent",
+        }}
+      >
+        {/* Down below you will need to write code for the Pause screen */}
+        {isPaused && (
+          <>
             <div className="flex items-center justify-center h-full">
-                <h1 className="text-4xl text-white">Game Paused</h1>
+              <h1 className="text-4xl text-white">Game Paused</h1>
             </div>
-        </div>
-        <GdLoader gdConfig={games[0]} />
-        </div>);
+          </>
+        )}
+      </div>
+      <GdLoader gdConfig={gameId} />
+    </div>
+  );
 }
