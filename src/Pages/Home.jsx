12b1f6b4ccf288f81e40ae36e8c88825f43513bf
aayush <aayushgoyaldps@gmail.com>
2024-07-18 05:09:18 +0530

    login

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-      <div>
-        <h1>Home</h1>
+      <div className="flex flex-col justify-center items-center text-white bg-black w-full h-[100vh]">
+        asdlasdlsak
       </div>
     );
 }
