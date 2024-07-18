b9759d7aac19b8d8f553766f1bc6f03583fd3613
aayush <aayushgoyaldps@gmail.com>
2024-07-19 01:20:15 +0530

    menu update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -37,7 +37,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center bg-black w-full h-[65vh] text-white">
+      <div className="flex flex-col items-center justify-center bg-black w-full h-[75vh] text-white">
         <img src={text} alt="" className="mb-10" />
         <div className="flex items-center space-y-4 text-xl">
           <div className="home_page_grid">
