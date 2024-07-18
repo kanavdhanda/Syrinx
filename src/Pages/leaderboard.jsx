3626c36df2c41149cd8831afdde7a00a81f37f0c
aayush <aayushgoyaldps@gmail.com>
2024-07-18 23:08:50 +0530

    index.css

--- src/Pages/leaderboard.jsx
+++ src/Pages/leaderboard.jsx
@@ -23,11 +23,50 @@ export default function Leaderboard() {
     useEffect(()=>{
         dataLao();
     },[])
+
+
+
+// test data
+    const teams = [
+        { rank: 5, name: 'HALLO', score: 1000 },
+        { rank: 6, name: 'TEAM1', score: 750 },
+        { rank: 7, name: 'TEAM2', score: 690 },
+        { rank: 8, name: 'TEAM3', score: 620 },
+        { rank: 9, name: 'TEAM4', score: 530 },
+      ];
+
+
+
   return (
+    
     <div className="flex flex-col items-center w-full h-[100vh]">
       <h1 className="text-5xl mb-5">Leaderboard</h1>
       <h1 className="ml-16 mt-8">TEAM 1</h1>
+      <h1 className="mt-12 mr-[380px]">TEAM 2</h1>
+      <h1 className="mt-9 ml-[400px]">TEAM 3</h1>
       <img src={leader} alt="" className="h-[220px] absolute mt-[80px]" />
+
+
+      <div className=" mt-20">
+      <table>
+        <thead>
+          <tr>
+            <th>RANK</th>
+            <th>TEAM NAME</th>
+            <th>SCORE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams.map((team) => (
+            <tr key={team.rank}>
+              <td>{team.rank}</td>
+              <td>{team.name}</td>
+              <td>{team.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
     </div>
   );
 }
