dde619a713e9b22d4f3528728ad4a943fd36ae38
aayush <aayushgoyaldps@gmail.com>
2024-07-20 02:29:07 +0530

    login changes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import "../Css/Login.css"
+import "../index.css"
+import text from "../assets/text.png"
 import logo from '../assets/logo.png';
+import char from '../assets/char.png';
+
 
 export default function Login() {
     const [formData, setFormData] = useState({
@@ -45,31 +50,38 @@ export default function Login() {
             [name]: value,
         });
     }
-
+//mb-4 w-64 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500
+//mb-4 w-64 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500
+//w-64 py-2 mt-4 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500
     return (
         <>
-            <div className="flex flex-col min-w-full h-[100vh] items-center justify-center">
-            {/* <img src={logo} alt="" className="w-40" /> */}
-                <form onSubmit={handleSubmit} className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg">
-                    <h1 className="mb-6 text-2xl font-bold text-gray-800">Login</h1>
+            <div className="flex flex-col gap-y-14 min-w-full h-[95vh] items-center justify-center">
+            <img src={text}></img>
+
+            <div className="grid grid-cols-2 gap-5">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-y-2 items-center p-6 bg-black rounded-lg shadow-lg">
+                    <h1 className="mb-6 text-6xl font-bold text-white">Login</h1>
                     <input
                         name="Username"
                         type="text"
                         placeholder="Username"
-                        className="mb-4 w-64 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className="input__fields focus:border-blue-500"
                         value={formData.Username}
                         onChange={handleChange}
+                        autoComplete="off"
                     />
                     <input
                         name="Password"
                         type="password"
                         placeholder="Password"
-                        className="mb-4 w-64 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className="input__fields"
                         value={formData.Password}
                         onChange={handleChange}
                     />
-                    <button type="submit" className="w-64 py-2 mt-4 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">Submit</button>
+                    <button type="submit" id="submit_btn">Submit</button>
                 </form>
+                <img src={char} alt="" className="mt-10" />
+                </div>
             </div>
         </>
     );
--- src/Pages/Rulebook.jsx
+++ src/Pages/Rulebook.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import scroll from '../assets/scroll2.png'
+
+const rulebook = ["1. The game will be played in a 5v5 format.","aslkdmalsdmaldlalasmdlaksmdsaldmska"]
 
 const Rulebook = () => {
   return (
-    <div className="flex justify-center items-center w-full h-[80vh]">
-        Rulebook
+    <div className="flex flex-col items-center w-full h-[80vh]">
+      <h1 className='text-5xl mb-5'>RULEBOOK</h1>
+      <img src={scroll} alt="" className='absolute mt-20' />
+      <div className=" ml-5 overflow-y-auto h-[380px] w-[490px] mt-28 text-black z-10">
+      <h1 className=' text-black z-10 text-2xl w-[490px]'>{rulebook}</h1>
+      </div>
     </div>
   )
 }
