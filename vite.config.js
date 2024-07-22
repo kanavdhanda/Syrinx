fc0adb15f7f367d4152782a312c37c7a676659d5
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-07-23 03:30:08 +0530

    more changes

--- vite.config.js
+++ vite.config.js
@@ -55,16 +55,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
-    },
-    server: {
-        headers: {
-            "Cross-Origin-Embedder-Policy": "require-corp",
-            "Cross-Origin-Opener-Policy": "same-origin",
-        },
-    },
-    publicDir: 'public',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  publicDir: 'public',
 });
