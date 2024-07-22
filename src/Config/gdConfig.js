4ec9800f7b323c81332ea3d4a47759c1eb413bd7
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-07-23 04:01:30 +0530

    yoyo

--- src/Config/gdConfig.js
+++ src/Config/gdConfig.js
@@ -18,7 +18,7 @@ const games = [
     name: "Level 1",
     args: [],
     canvasResizePolicy: 2,
-    executable: "/level-1/level-1/level-1",
+    executable: "/level-1/level-1",
     // executable:"/api/proxy/level-1",
     // executable: "https://srinx-ccs.blr1.cdn.digitaloceanspaces.com/level-1/level-1",
     experimentalVK: true,
