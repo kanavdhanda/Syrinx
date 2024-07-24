36e1ff791254d0128f8652342f5060951f406bc0
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-07-24 18:23:27 +0530

    imporvements to auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import axios from 'axios';
@@ -6,36 +6,43 @@ import axios from 'axios';
 export default function PrivateRoute({ children }) {
     const navigate = useNavigate();
     const token = Cookies.get('token');
-    const dataLao = async(token1)=>{
-        try{
-          const tokenString = token1.split(",");
-          
-          const sesId = tokenString.map(item => parseInt(item, 10));
-    
-          const response = await axios.post("http://127.0.0.1:8080/teaminfo",{
-            SessionID : sesId
-          }
-          )
-          console.log(response.data);
-          return response.data;
+    const [isAuthorized, setIsAuthorized] = useState(false);
+
+    const dataLao = async (token1) => {
+        try {
+            const tokenString = token1.split(",");
+            const sesId = tokenString.map(item => parseInt(item, 10));
+
+            const response = await axios.post("http://127.0.0.1:8080/teaminfo", {
+                SessionID: sesId
+            });
+
+            console.log(response.data);
+            return response.data;
+        } catch (e) {
+            console.error("Error fetching data:", e);
+            return null;
         }
-        catch(e){
-          console.error("Error fetching data:", e)
-        }
-      }
+    }
+
     useEffect(() => {
-        if (!token) {
-            navigate('/');
-        }
-        else {
-            const data = dataLao(token);
-            if(!data){
+        const verifyToken = async () => {
+            if (!token) {
                 navigate('/');
+            } else {
+                const data = await dataLao(token);
+                if (!data) {
+                    navigate('/');
+                } else {
+                    setIsAuthorized(true);
+                }
             }
         }
-    }, [navigate, token]); 
 
-    if (!token) {
+        verifyToken();
+    }, [navigate, token]);
+
+    if (!isAuthorized) {
         return null;
     }
 
