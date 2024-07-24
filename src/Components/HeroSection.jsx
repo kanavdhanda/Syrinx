705e8fa5ca4c72d56cfbaf8e9f629625516d2211
aayush <aayushgoyaldps@gmail.com>
2024-07-25 04:07:48 +0530

    gradients added

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -17,10 +17,10 @@ const HeroSection = () => {
       */}
 
       <div className="absolute inset-0 ">
-        <div className='h-[25%] mt-[17%] flex items-center justify-center text-white text-center flex-col'>
+        <div className='h-[20%] mt-[17%] flex items-center justify-center text-white text-center flex-col'>
         <img src={ccs} className=' w-16 lg:w-24 mb-3' alt="" />
         <h2 className='mb-10 text-xl lg:text-3xl shadow- shadow-yellow-300 content'>presents</h2>
-        <img src={text} alt="" />
+        <img src={text} alt=""  />
         </div>
       </div>
     </div>
--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -2,6 +2,7 @@ import text from '../assets/text.png';
 import Coverpage from '../Components/Coverpage';
 import HeroSection from '../Components/HeroSection';
 import SponserArea from '../Components/SponserArea';
+import '../Css/mainpage.css'
 
 
 export default function MainPage() {
@@ -10,22 +11,24 @@ export default function MainPage() {
     <div className="crt-screen min-h-screen flex flex-col justify-center items-center" >
       
       <div className=" w-full " >
-        <div className=" w-full mb-[10vh] h-[70%]">
+        <div className=" w-full mb-[8vh]  h-[70%]">
+          <div className="bggrad123 z-10 h-[20vh] "></div>
           <HeroSection  className="hero-section"/>
         </div>
         <div className="crt-content">
         <div  className="flex gap-10 flex-col content w-[90%] lg:w-[80%] px-4 lg:px-8 py-8  max-w-screen-xl mx-auto lg:py-16  random-flicker">
           
             <div className='mt-9 mb-9' style={{ height: '100vh' }}>
+            
+              <SponserArea />
+            </div>
 
-  <SponserArea />
-</div>
 
-
-    </div>
-        </div>
-        <div className="w-30px mt-[20vh] random-flicker flex-col" id='Register' >
-          <Coverpage/>
+              </div>
+            </div>
+            <div className="w-30px mt-[15vh] random-flicker flex-col" id='Register' >
+            <div className="bggrad456 z-100 h-[20vh] "></div>
+              <Coverpage/>
         </div>
         </div>
       </div>
